9519d356e6ff09a0ab3b80474ddc1b41cb20f1ce
pandacodes1 <107660624+pandacodes1@users.noreply.github.com>
2022-06-24 15:12:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Ojo Alafia David";
+document.getElementById("height").innerHTML = "5 foot 7 inches";
+document.getElementById("nationality").innerHTML = "Nigerian";
